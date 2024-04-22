1d1df54e5c88df2e5a0b45b5d62a3d883a8b9866
Saltssaumure <Saltssaumure@users.noreply.github.com>
2024-04-22 03:56:44 +0000

    deploy: a23ddcc3eb1ceff7790f05c38b854e1e754c0220

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "Neobrutal" /* The official name of your theme. */,
+        scss: "brutal" /* The name of your base scss file. */,
+        repo: "neobrutal-discord-theme" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
